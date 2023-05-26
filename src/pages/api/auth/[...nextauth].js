d80ce1a8495b1d73a92c85f478b48996d73bf422
Cartagena2001 <salvadorcartajena@hotmail.com>
2023-05-25 21:03:50 -0600

    add secret key

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth/next";
 import GoogeProvider from "next-auth/providers/google";
 
 export const authOptions = {
+    secret: process.env.SECRET,
     providers: [
         GoogeProvider({
             clientId: process.env.GOOGLE_CLIENT_ID,
